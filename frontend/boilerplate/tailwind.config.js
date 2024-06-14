1d8fc5fdfcdb0bf57f9d63674b39d0fdbf148552
Denis Ronchese <denis.ronchese@gmail.com>
2024-06-15 00:11:35 +0200

    Setup cleared for shadcn/ui and magicui

--- frontend/boilerplate/tailwind.config.js
+++ frontend/boilerplate/tailwind.config.js
@@ -1,9 +1,12 @@
 const designTokens = require('./src/designTokens');
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 module.exports = {
   prefix: 'tw-', // Ensures no conflict with MUI classes
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", // Include all relevant paths
+    "app/**/*.{ts,tsx}", // Add paths from shadcn/ui setup
+    "components/**/*.{ts,tsx}" // Add paths from shadcn/ui setup
   ],
   theme: {
     extend: {
@@ -24,12 +27,71 @@ module.exports = {
         downvote: designTokens.colors.downvote,
         containerPrimary: designTokens.colors.containerPrimary,
         containerSecondary: designTokens.colors.containerSecondary,
+
+        // Add colors from shadcn/ui setup
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
         primary: ['Roboto', 'Helvetica', 'Arial', 'sans-serif'],
         secondary: ['Lato', 'sans-serif'],
+
+        // Add font family from shadcn/ui setup
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-animate"), // Add plugin from shadcn/ui setup
+  ],
 };
