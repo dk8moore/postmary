6d051d31f2e6b0c74cd11e1194e48e41c2a796a9
Denis Ronchese <denis.ronchese@gmail.com>
2024-06-16 23:47:38 +0200

    Additional components for landing page

--- frontend/boilerplate/tailwind.config.js
+++ frontend/boilerplate/tailwind.config.js
@@ -3,7 +3,11 @@ const { fontFamily } = require("tailwindcss/defaultTheme")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: ["./src/app/**/*.{ts,tsx}", ".src/components/**/*.{ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}",],
+  content: [
+    "./src/app/**/*.{ts,tsx}",
+    ".src/components/**/*.{ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     container: {
       center: true,
