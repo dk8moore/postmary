e2315f6a00e805b93de61a8b1a89000cd3254403
Denis Ronchese <denis.ronchese@gmail.com>
2024-07-07 16:15:20 +0200

    Add api paths

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -3,6 +3,7 @@ module.exports = {
   webpack: {
     alias: {
         '@': path.resolve(__dirname, 'src'),
+        '@api': path.resolve(__dirname, 'src/api'),
         '@app': path.resolve(__dirname, 'src/components/app'),
         '@asset': path.resolve(__dirname, 'src/assets'),   
         '@common': path.resolve(__dirname, 'src/components/common'),
